1ac621d58e877cef0fbc85ecb2124d48f5ed5110
MohamedSalamaMouse <slamtm608@gmail.com>
2023-04-12 03:47:40 +0200

    Latest Listings

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     function  index(){
-        return view('frontend.search');
+        $properties=Property::all();
+        return view('frontend.search',compact('properties'));
     }
 }
--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -127,91 +127,86 @@
 <!-- listings section starts  -->
 <section class="listings">
 
-   <h1 class="heading">searched results</h1>
 
+    <h1 class="heading">latest listings</h1>
    <div class="box-container">
 
-      <div class="box">
-         <div class="admin">
-            <h3>j</h3>
-            <div>
-               <p>john deo</p>
-               <span>10-11-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <p class="total-images"><i class="far fa-image"></i><span>4</span></p>
-            <p class="type"><span>house</span><span>sale</span></p>
-            <form action="" method="post" class="save">
-               <button type="submit" name="save" class="far fa-heart"></button>
-            </form>
-            <img src="images/house-img-1.webp" alt="">
-         </div>
-         <h3 class="name">modern flats and appartments</h3>
-         <p class="location"><i class="fas fa-map-marker-alt"></i><span>andheri, mumbai, india - 401303</span></p>
-         <div class="flex">
-            <p><i class="fas fa-bed"></i><span>3</span></p>
-            <p><i class="fas fa-bath"></i><span>2</span></p>
-            <p><i class="fas fa-maximize"></i><span>750sqft</span></p>
-         </div>
-         <a href="view_property.html" class="btn">view property</a>
-      </div>
+       @if ($properties != [])
+           @foreach ($properties as $property)
 
-      <div class="box">
-         <div class="admin">
-            <h3>j</h3>
-            <div>
-               <p>john deo</p>
-               <span>10-11-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <p class="total-images"><i class="far fa-image"></i><span>4</span></p>
-            <p class="type"><span>flat</span><span>sale</span></p>
-            <form action="" method="post" class="save">
-               <button type="submit" name="save" class="far fa-heart"></button>
-            </form>
-            <img src="images/house-img-2.webp" alt="">
-         </div>
-         <h3 class="name">modern flats and appartments</h3>
-         <p class="location"><i class="fas fa-map-marker-alt"></i><span>andheri, mumbai, india - 401303</span></p>
-         <div class="flex">
-            <p><i class="fas fa-bed"></i><span>3</span></p>
-            <p><i class="fas fa-bath"></i><span>2</span></p>
-            <p><i class="fas fa-maximize"></i><span>750sqft</span></p>
-         </div>
-         <a href="view_property.html" class="btn">view property</a>
-      </div>
 
-      <div class="box">
-         <div class="admin">
-            <h3>j</h3>
-            <div>
-               <p>john deo</p>
-               <span>10-11-2022</span>
-            </div>
-         </div>
-         <div class="thumb">
-            <p class="total-images"><i class="far fa-image"></i><span>4</span></p>
-            <p class="type"><span>flat</span><span>sale</span></p>
-            <form action="" method="post" class="save">
-               <button type="submit" name="save" class="far fa-heart"></button>
-            </form>
-            <img src="images/house-img-3.jpg" alt="">
-         </div>
-         <h3 class="name">modern flats and appartments</h3>
-         <p class="location"><i class="fas fa-map-marker-alt"></i><span>andheri, mumbai, india - 401303</span></p>
-         <div class="flex">
-            <p><i class="fas fa-bed"></i><span>3</span></p>
-            <p><i class="fas fa-bath"></i><span>2</span></p>
-            <p><i class="fas fa-maximize"></i><span>750sqft</span></p>
-         </div>
-         <a href="view_property.html" class="btn">view property</a>
-      </div>
+               @if(!empty($property->image_02))
+                       <?php $image_coutn_02 = 1?>
+               @else
+                       <?=$image_coutn_02 = 0?>
+               @endif
+               @if(!empty($property->image_03))
+                       <?php $image_coutn_03 = 1?>
+               @else
+                       <?=$image_coutn_03 = 0?>
+               @endif
+               @if(!empty($property->image_04))
+                       <?php $image_coutn_04 = 1?>
+               @else
+                       <?=$image_coutn_04 = 0?>
+               @endif
+               @if(!empty($property->image_05))
+                       <?php $image_coutn_05 = 1?>
+               @else
+                       <?=$image_coutn_05 = 0?>
+               @endif
+
+                   <?php $total_images = (1 + $image_coutn_02 + $image_coutn_03 + $image_coutn_04 + $image_coutn_05);?>
+
+
+               <form action="" method="POST">
+                   <div class="box">
+                       <input type="hidden" name="property_id" value="{{$property->id}}">
+
+                       <button type="submit" name="save" class="save"><i class="fas fa-heart"></i><span>saved</span></button>
+
+                       <button type="submit" name="save" class="save"><i class="far fa-heart"></i><span>save</span></button>
+
+                       <div class="thumb">
+                           <p class="total-images"><i class="far fa-image"></i><span><?= $total_images; ?></span></p>
+
+                           <img src="attachments/{{$property->user_id}}/{{$property->id}}/{{$property->image_01}}" alt="">
+                       </div>
+                       <div class="admin">
+                           <h3><?= substr($property->user_name, 0, 1); ?></h3>
+                           <div>
+                               <p>{{$property->user_name}}</p>
+                               <span>{{$property->created_at}}</span>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="box">
+                       <div class="price"><i class="fas fa-indian-rupee-sign"></i><span>{{$property->price}}</span></div>
+                       <h3 class="name">{{$property->property_name}}</h3>
+                       <p class="location"><i class="fas fa-map-marker-alt"></i><span>{{$property->address}}</span></p>
+                       <div class="flex">
+                           <p><i class="fas fa-house"></i><span>{{$property->type}}</span></p>
+                           <p><i class="fas fa-tag"></i><span>{{$property->offer}}</span></p>
+                           <p><i class="fas fa-bed"></i><span>{{$property->bnk}}</span></p>
+                           <p><i class="fas fa-trowel"></i><span>{{$property->status}}</span></p>
+                           <p><i class="fas fa-couch"></i><span>{{$property->furnished}}</span></p>
+                           <p><i class="fas fa-maximize"></i><span>{{$property->carpet}}sqft</span></p>
+                       </div>
+                       <div class="flex-btn">
+                           <a href="{{route('view_property',$property->id)}}" class="btn">view property</a>
+                           <input type="submit" value="send enquiry" name="send" class="btn">
+                       </div>
+                   </div>
+               </form>
+
+           @endforeach
+       @else
+           <p class="empty">no properties added yet! <a href="post_property.php" style="margin-top:1.5rem;" class="btn">add new</a></p>
+       @endif
 
    </div>
 
-</section>
+
 <!-- listings section ends -->
 @endsection
 @section('script')
