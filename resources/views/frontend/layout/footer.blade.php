6501f6afa60946debd4b67966207d5ac8b9581b1
MohamedSalamaMouse <slamtm608@gmail.com>
2023-04-24 16:11:41 +0200

    edit in footerr

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -10,18 +10,18 @@
         </div>
 
         <div class="box">
-            <a href="home.html"><span>home</span></a>
-            <a href="about.html"><span>about</span></a>
-            <a href="contact.html"><span>contact</span></a>
-            <a href="listings.html"><span>all listings</span></a>
-            <a href="#"><span>saved properties</span></a>
+            <a href="{{route('home_page')}}"><span>home</span></a>
+            <a href="{{route('contact.about')}}"><span>about</span></a>
+            <a href="{{route('contact')}}"><span>contact</span></a>
+            <a href="{{route('all listings')}}"><span>all listings</span></a>
+
         </div>
 
         <div class="box">
             <a href="https://www.facebook.com/profile.php?id=100018619843475"  target="_blank"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
             <a href="https://twitter.com/slamtm608"  target="_blank"><span>twitter</span><i class="fab fa-twitter"></i></a>
             <a href="https://www.linkedin.com/in/mohamed-salama-491877200/"  target="_blank"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
-            <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
 
         </div>
 
